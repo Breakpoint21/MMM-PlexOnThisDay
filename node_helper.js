f8f75b967c489c335a9ff699da680b10f5f46c64
Pascal Schumann <pschumann.mail@googlemail.com>
2021-05-03 21:34:21 +0200

    delete demo functions

--- node_helper.js
+++ node_helper.js
@@ -47,26 +47,6 @@ module.exports = NodeHelper.create({
 		}
 	},
 
-	// Example function send notification test
-	sendNotificationTest: function(payload) {
-		this.sendSocketNotification("MMM-PlexOnThisDay-NOTIFICATION_TEST", payload);
-	},
-
-	// this you can create extra routes for your module
-	extraRoutes: function() {
-		var self = this;
-		this.expressApp.get("/MMM-PlexOnThisDay/extra_route", function(req, res) {
-			// call another function
-			values = self.anotherFunction();
-			res.send(values);
-		});
-	},
-
-	// Test another function
-	anotherFunction: function() {
-		return {date: new Date()};
-	},
-
 	getPhotoUrl: function (plexPhoto) {
 		return `http://${api.hostname}:${api.port}${plexPhoto.Media[0].Part[0].key}?X-Plex-Token=${api.authToken}`;
 	},
